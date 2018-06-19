f4d3e74966f1f952642c862977b30959f0e6da54
Richard Jiang <rmjiang7@gmail.com>
2018-06-19 12:54:25 -0700

    Added multiprocessing to skm

--- bayes_lib/apps/skm.py
+++ bayes_lib/apps/skm.py
@@ -1,9 +1,51 @@
 import numpy as np
 import abc
 from scipy.stats import norm
+import pathos.pools as pp
 
 from ..rvs import RandomVariable, get_rv_value
 import time
+
+def gillespie_simulate_par(initial_state, reactions, rate_fx, times, end_T = None):
+        states = [np.hstack((np.array([0]),initial_state))]
+        if times is not None:
+            times = times[1:]
+            end_T = max(times)
+        cur_state = states[0]
+        terminate = False
+        step_counter = 0
+        while not terminate and cur_state[0] <= end_T:
+            if step_counter > 200000:
+                print("Took too long!")
+                return None
+            updated_state = np.zeros(initial_state.shape[0] + 1)
+            rates = rate_fx(cur_state)
+            total_rate = sum(rates)
+            if not np.isfinite(total_rate):
+                print(cur_state)
+                print("Total rate is infinite!")
+                return None
+            if total_rate == 0:
+                tau = np.inf
+            else:
+                tau = np.random.exponential(1/total_rate)
+                event = np.random.choice(rates.shape[0], p = rates/total_rate)
+                step_counter += 1
+
+                t = reactions[event,:]
+                updated_state[1:] = cur_state[1:] + t
+            updated_state[0] = cur_state[0] + tau
+            if times is not None:
+                while not terminate and updated_state[0] > times[0]:
+                    states.append(np.append(times[0], cur_state[1:]))
+                    times = times[1:]
+                    if len(times) == 0:
+                        terminate = True
+            else:
+                states.append(updated_state)
+            cur_state = updated_state
+            #print("Steps Taken: ", step_counter)
+        return np.vstack(states)
 
 class StochasticKineticRandomVariable(RandomVariable):
 
@@ -24,12 +66,13 @@ class StochasticKineticRandomVariable(RandomVariable):
         return norm.logpdf(obs, particle, 5)
 
     def log_density(self, n_smc = 50, v = None):
+        p = pp.ProcessPool()
         if v is None:
             v = self.cvalue
             jd = self.jdet
         
         rate_fx = self.gen_rate_fx()
-        
+
         T = self.times.shape[0]
         if len(v.shape) == 1:
             N = 1
@@ -49,7 +92,7 @@ class StochasticKineticRandomVariable(RandomVariable):
         for i in range(1,T):
             resampled_indices = np.random.choice(list(range(n_smc)), n_smc, p = weights, replace = True)
             resampled_particles = particles[resampled_indices, :, i-1]
-            res = list(map(lambda x: self.gillespie_simulate(x, self.reactions, rate_fx, np.array([0,deltas[i-1]])), resampled_particles))
+            res = list(p.map(lambda x: gillespie_simulate_par(x, self.reactions, rate_fx, np.array([0,deltas[i-1]])), resampled_particles))
             new_weights = [-np.inf if r is None else self.obs_log_likelihood(v[i], r[1,1:]) for r in res]
             particles[:,:,i] = np.array([0 if r is None else r[1,1:] for r in res])
             n = np.exp(new_weights)
@@ -61,11 +104,10 @@ class StochasticKineticRandomVariable(RandomVariable):
             weights = weights[:,0]
             llhood_est += np.mean(new_weights)
         return llhood_est
-    
+
     def gillespie_simulate(self, initial_state, reactions, rate_fx, times = None, tau_fn = None):
         if times is None:
             times = self.times
-        step_counter = 0
         if tau_fn:
             states = [np.hstack((np.array([0]),initial_state))]
             if times is not None:
@@ -96,42 +138,5 @@ class StochasticKineticRandomVariable(RandomVariable):
                 states.append(updated_state)
                 cur_state = updated_state
             return np.vstack(states)
-        else: 
-            states = [np.hstack((np.array([0]),initial_state))]
-            if times is not None:
-                times = times[1:]
-                self.end_T = max(times)
-            cur_state = states[0]
-            terminate = False
-            while not terminate and cur_state[0] < self.end_T:
-                if step_counter > 200000:
-                    print("Took too long!")
-                    return None
-                updated_state = np.zeros(initial_state.shape[0] + 1)
-                rates = rate_fx(cur_state)
-                total_rate = sum(rates)
-                if not np.isfinite(total_rate):
-                    print(cur_state)
-                    print("Total rate is infinite!")
-                    return None
-                if total_rate == 0:
-                    tau = np.inf
-                else:
-                    tau = np.random.exponential(1/total_rate)
-                    event = np.random.choice(rates.shape[0], p = rates/total_rate)
-                    step_counter += 1
-
-                    t = reactions[event,:]
-                    updated_state[1:] = cur_state[1:] + t
-                updated_state[0] = cur_state[0] + tau
-                if times is not None:
-                    while not terminate and updated_state[0] > times[0]:
-                        states.append(np.append(self.times[0], cur_state[1:]))
-                        times = times[1:]
-                        if len(times) == 0:
-                            terminate = True
-                else:
-                    states.append(updated_state)
-                cur_state = updated_state
-            #print("Steps Taken: ", step_counter)
-            return np.vstack(states)
+        else:
+            return gillespie_simulate_par(initial_state, reactions, rate_fx, times, end_T = self.end_T)
